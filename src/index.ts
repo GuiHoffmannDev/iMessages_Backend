95a1c9fc81eaf0354f3e069b39182164da06a0d1
Vadim Savin <savinvadim1312@gmail.com>
2023-07-07 13:01:05 +0100

    auth

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import authRoutes from './routes/authRoutes';
+
+const app = express();
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+app.listen(3000, () => {
+  console.log(`Server ready at: http://localhost:3000`);
+});
